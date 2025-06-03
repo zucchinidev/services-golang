cfa4b0ad2ff3a88f5811a8acc9309945651888e9
Andrea Zucchini <andreazucchini@mac.home>
2025-06-03 16:52:10 +0100

    feat: implement structured configuration for sales service

--- apis/services/sales/main.go
+++ apis/services/sales/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 	"runtime"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/zucchini/services-golang/foundation/logger"
 )
+
+var buildRef = "development"
 
 func main() {
 	var log *logger.Logger
@@ -42,6 +48,48 @@ func run(ctx context.Context, log *logger.Logger) error {
 
 	// -------------------------------------------------------------------------
 	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout        time.Duration `conf:"default:5s"`
+			WriteTimeout       time.Duration `conf:"default:10s"`
+			IdleTimeout        time.Duration `conf:"default:120s"`
+			ShutdownTimeout    time.Duration `conf:"default:20s"`
+			APIHost            string        `conf:"default:0.0.0.0:3000"`
+			DebugHost          string        `conf:"default:0.0.0.0:3010"`
+			CORSAllowedOrigins []string      `conf:"default:*,mask"`
+		}
+	}{
+		Version: conf.Version{
+			Build: buildRef,
+			Desc:  "Sales",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", cfg.Build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	// -------------------------------------------------------------------------
 
 	return nil
 }
