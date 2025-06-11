2417ecc7d69140b58dde7a0b69e66efcb05ca30e
Andrea Zucchini <zucchinidev@protonmail.com>
2025-06-11 16:24:14 +0100

    feat: integrate error validation and shutdown signal in request handler

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -2,9 +2,10 @@ package web
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"net/http"
 	"os"
+	"syscall"
 	"time"
 
 	"github.com/google/uuid"
@@ -34,6 +35,11 @@ func NewApp(shutdown chan os.Signal, mw ...MidHandler) *App {
 	}
 }
 
+// SignalShutdown is used to gracefully shutdown the app when an integrity issue is identified.
+func (a *App) SignalShutdown() {
+	a.shutdown <- syscall.SIGTERM
+}
+
 func (a *App) HandleFunc(pattern string, handler Handler, mw ...MidHandler) {
 	// local middleware first
 	// This allows us to for example, add an authentication middleware only to this handler.
@@ -55,13 +61,67 @@ func (a *App) HandleFunc(pattern string, handler Handler, mw ...MidHandler) {
 		ctx := setValues(r.Context(), v)
 
 		if err := handler(ctx, w, r); err != nil {
-			// Temporary logging.
-			fmt.Println("web.HandleFunc: %w", err)
-			return
+
+			// This error could happen when we send the shutdown signal or we cannot write down to the pipe.
+			// This is because the manage the errors down to the handler level.
+
+			if validateError(err) {
+				// We prefer to shutdown the server gracefully
+				// rather than have the server in an inconsistent state.
+				// It is a tough call, but I think this is the best choice.
+				// It is better to restart than to have corrumpted file systems, data, etc.
+				a.SignalShutdown()
+				return
+			}
 		}
 
 		// Put any code here that needs to happen after the handler is called.
 	}
 
 	a.ServeMux.HandleFunc(pattern, h)
+}
+
+// validateError validates the error for special conditions that do not
+// warrant an actual shutdown by the system.
+func validateError(err error) bool {
+
+	// Ignore syscall.EPIPE and syscall.ECONNRESET errors which occurs
+	// when a write operation happens on the http.ResponseWriter that
+	// has simultaneously been disconnected by the client (TCP
+	// connections is broken). For instance, when large amounts of
+	// data is being written or streamed to the client.
+	// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+	// https://gosamples.dev/broken-pipe/
+	// https://gosamples.dev/connection-reset-by-peer/
+
+	// These errors are explicitly ignored (function returns false) because they represent client-side disconnections rather than server-side problems. They are:
+	// Routine network issues that happen frequently in production systems
+	// Not indicative of application logic problems
+	// Expected behaviors when clients disconnect unexpectedly
+	// Handled automatically by the TCP/IP stack
+
+	switch {
+	case errors.Is(err, syscall.EPIPE):
+		// Usually, you get the broken pipe error when you write to the connection after the
+		// RST (TCP RST Flag) is sent.
+		// The broken pipe is a TCP/IP error occurring when you write to a stream where the
+		// other end (the peer) has closed the underlying connection. The first write to the
+		// closed connection causes the peer to reply with an RST packet indicating that the
+		// connection should be terminated immediately. The second write to the socket that
+		// has already received the RST causes the broken pipe error.
+		return false
+
+	case errors.Is(err, syscall.ECONNRESET):
+		// Usually, you get connection reset by peer error when you read from the
+		// connection after the RST (TCP RST Flag) is sent.
+		// The connection reset by peer is a TCP/IP error that occurs when the other end (peer)
+		// has unexpectedly closed the connection. It happens when you send a packet from your
+		// end, but the other end crashes and forcibly closes the connection with the RST
+		// packet instead of the TCP FIN, which is used to close a connection under normal
+		// circumstances.
+		return false
+	}
+
+	return true
+
 }
