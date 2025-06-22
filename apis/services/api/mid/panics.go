1e4c563417c0f4246a515cb2c981dd1203a480d9
Andrea Zucchini <zucchinidev@protonmail.com>
2025-06-21 21:09:17 -0500

    feat(api): add HTTP panic middleware adapter
    - Add HTTP-specific panic middleware in api/mid package
    - Adapt core panic middleware for web handlers
    - Bridge between protocol-agnostic and HTTP-specific implementations

---
+++ apis/services/api/mid/panics.go
@@ -0,0 +1,22 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/zucchini/services-golang/app/mid"
+	"github.com/zucchini/services-golang/foundation/logger"
+	"github.com/zucchini/services-golang/foundation/web"
+)
+
+// Panics is a middleware that recovers from panics and returns an error so it
+// can be reported in Metrics and handled in Errors
+func Panics(log *logger.Logger) web.MidHandler {
+	return func(next web.Handler) web.Handler {
+		return func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			return mid.Panics(ctx, func(ctx context.Context) error {
+				return next(ctx, w, r)
+			})
+		}
+	}
+}
